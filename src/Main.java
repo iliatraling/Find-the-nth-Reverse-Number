31794aed950f6d963a9908d150bf1c1cd7885875
iliatraling <iliatraling35@gmail.com>
2023-12-01 13:27:56 +0300

    Created Update metod

--- src/Main.java
+++ src/Main.java
@@ -17,25 +17,40 @@ public class Main {
         long timeStart, timeEnd;
         BigInteger reversNumber;
         timeStart = System.currentTimeMillis();
-        reversNumber = findReverseNumberSimple(10_000);
+        reversNumber = findReverseNumberUpdate(10_000);
         timeEnd = System.currentTimeMillis();
         System.out.println("Simple metod execution time = " + (timeEnd - timeStart) + " ms. Revers Number is " + reversNumber);
 
     }
-    public static BigInteger findReverseNumberSimple(long n) {
-        long answer = 0;
-        for (long i = 0, count = 1; count <= n; i++) {
+    public static BigInteger findReverseNumberUpdate(long n) {
+        boolean isOdd = true;
+        StringBuilder answer;
+        long centerNumber = 11;// first part of the polydrome number
+        long koef = 10;//the coefficient that must be subtracted changes when the length of the polydrome turns from odd to even
 
-            String str = Long.toString(i);
-            String strReverse = new StringBuilder(str).reverse().toString();
-            if (str.equals(strReverse)) {
-                //System.out.println(str + " " + count);
-                count++;
-                answer = i;
+
+
+
+        if(n < 11)
+            return new BigInteger(Long.toString(n - 1));
+
+        for (long i = 11; i <= n; i++) {
+            if(Long.toString(centerNumber).length() < Long.toString(centerNumber+1).length()){
+                isOdd = !isOdd;
+                if(isOdd) {
+                    koef *= 10;
+                }
             }
-
+            centerNumber = i - koef; // при всех случаях
         }
-        //System.out.println(" ");
-        return new BigInteger(Long.toString(answer));
+
+        if(isOdd) { // create full number (revers first part of number)
+            answer = new StringBuilder(Long.toString(centerNumber)).append(new StringBuilder(Long.toString(centerNumber)).reverse());
+        } else {
+            StringBuilder stringBuilder = new StringBuilder(Long.toString(centerNumber));
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            answer = new StringBuilder(Long.toString(centerNumber)).append(stringBuilder.reverse());
+        }
+        return new BigInteger(answer.toString());
     }
 }
